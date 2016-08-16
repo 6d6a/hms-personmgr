c3ba6db91daec7a1f526c48d5486bc6e263f5838
zaborshikov <zaborshikov@majordomo.ru>
2016-08-16 16:27:13 +0300

    last

--- src/main/java/ru/majordomo/hms/personmgr/Application.java
+++ src/main/java/ru/majordomo/hms/personmgr/Application.java
@@ -1,18 +1,25 @@
 package ru.majordomo.hms.personmgr;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+    @Bean
+    private static Logger logger() {
+        return LoggerFactory.getLogger(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     public void run(String... args) {
-
+        logger().info("Launched personal manager");
     }
 }
--- src/main/java/ru/majordomo/hms/personmgr/Test/ResponseTest.java
+++ src/main/java/ru/majordomo/hms/personmgr/Test/ResponseTest.java
@@ -1,6 +1,6 @@
 package ru.majordomo.hms.personmgr.Test;
 
-import ru.majordomo.hms.personmgr.controller.Response;
+import ru.majordomo.hms.personmgr.models.Response;
 
 public class ResponseTest {
 
