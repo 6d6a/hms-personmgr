68eb62bc5429f8fdaf47152c2035649b668db072
Ilya Dolnigin <dolnigin@majordomo.ru>
2016-09-28 11:17:38 +0300

    Saving  PersonalAccount "credit"-setting when importing from DB

--- src/main/java/ru/majordomo/hms/personmgr/service/importing/PersonalAccountDBImportService.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/importing/PersonalAccountDBImportService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,24 +33,23 @@ public class PersonalAccountDBImportService {
     }
 
     private void pull() {
-        String query = "SELECT a.id, a.name, a.client_id, m.notify_days FROM account a LEFT JOIN Money m ON a.id = m.acc_id";
-        personalAccounts = jdbcTemplate.query(query, (rs, rowNum) -> {
-            PersonalAccount personalAccount = new PersonalAccount(rs.getString("id"), rs.getString("client_id"), rs.getString("name"), AccountType.VIRTUAL_HOSTING);
-            personalAccount.setSetting("notify_days", rs.getString("notify_days"));
-            return personalAccount;
-        });
+        String query = "SELECT a.id, a.name, a.client_id, a.credit, m.notify_days FROM account a LEFT JOIN Money m ON a.id = m.acc_id";
+        personalAccounts = jdbcTemplate.query(query, this::rowMap);
     }
 
     private void pull(String accountName) {
-        String query = "SELECT a.id, a.name, a.client_id, m.notify_days FROM account a LEFT JOIN Money m ON a.id = m.acc_id WHERE name = ?";
+        String query = "SELECT a.id, a.name, a.client_id, a.credit, m.notify_days FROM account a LEFT JOIN Money m ON a.id = m.acc_id WHERE name = ?";
         personalAccounts = jdbcTemplate.query(query,
                 new Object[] { accountName },
-                (rs, rowNum) -> {
-                    PersonalAccount personalAccount = new PersonalAccount(rs.getString("id"), rs.getString("client_id"), rs.getString("name"), AccountType.VIRTUAL_HOSTING);
-                    personalAccount.setSetting("notify_days", rs.getString("notify_days"));
-                    return personalAccount;
-                }
+                this::rowMap
         );
+    }
+
+    private PersonalAccount rowMap(ResultSet rs, int rowNum) throws SQLException {
+        PersonalAccount personalAccount = new PersonalAccount(rs.getString("id"), rs.getString("client_id"), rs.getString("name"), AccountType.VIRTUAL_HOSTING);
+        personalAccount.setSetting("notify_days", rs.getString("notify_days"));
+        personalAccount.setSetting("credit", rs.getString("credit").equals("y") ? "1" : "0");
+        return personalAccount;
     }
 
     public boolean importToMongo() {
