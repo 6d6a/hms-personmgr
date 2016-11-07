3499f6c3bdda041c43e7042b12373baa04746a77
Ilya Dolnigin <dolnigin@majordomo.ru>
2016-11-07 16:26:31 +0300

    tld services cache + "зимние заготовки" для создания аккаунта si/fin

--- src/main/java/ru/majordomo/hms/personmgr/Application.java
+++ src/main/java/ru/majordomo/hms/personmgr/Application.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +29,7 @@ import ru.majordomo.hms.personmgr.service.importing.*;
 })
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCaching
 public class Application implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
