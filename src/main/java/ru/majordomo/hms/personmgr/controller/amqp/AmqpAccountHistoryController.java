e490886a7563502f53d0611d1ba12f7818fab29a
Илья Долныгин <dolnigin@majordomo.ru>
2016-11-23 17:23:29 +0300

    Добавлены REST и AMQP контроллер для accountHistory

---
+++ src/main/java/ru/majordomo/hms/personmgr/controller/amqp/AmqpAccountHistoryController.java
@@ -0,0 +1,41 @@
+package ru.majordomo.hms.personmgr.controller.amqp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import ru.majordomo.hms.personmgr.common.message.SimpleServiceMessage;
+import ru.majordomo.hms.personmgr.service.AccountHistoryService;
+
+import java.util.Map;
+
+@EnableRabbit
+@Service
+public class AmqpAccountHistoryController {
+
+    private final static Logger logger = LoggerFactory.getLogger(AmqpAccountHistoryController.class);
+
+    private final AccountHistoryService accountHistoryService;
+
+    @Autowired
+    public AmqpAccountHistoryController(AccountHistoryService accountHistoryService) {
+        this.accountHistoryService = accountHistoryService;
+    }
+
+    @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "pm.account-history", durable = "true", autoDelete = "true"), exchange = @Exchange(value = "account-history", type = ExchangeTypes.TOPIC), key = "pm"))
+    public void create(@Payload SimpleServiceMessage message, @Headers Map<String, String> headers) {
+        String provider = headers.get("provider");
+        logger.info("Received from " + provider + ": " + message.toString());
+
+        String historyMessage = (String) message.getParam("historyMessage");
+        String operator = (String) message.getParam("operator");
+
+        if (historyMessage != null && operator != null) {
+            accountHistoryService.addMessage(message.getAccountId(), historyMessage, operator);
+        }
+    }
+}
--- src/main/java/ru/majordomo/hms/personmgr/repository/AccountHistoryRepository.java
+++ src/main/java/ru/majordomo/hms/personmgr/repository/AccountHistoryRepository.java
@@ -1,6 +1,8 @@
 package ru.majordomo.hms.personmgr.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +14,6 @@ public interface AccountHistoryRepository extends MongoRepository<AccountHistory
     AccountHistory findOne(String id);
     List<AccountHistory> findAll();
     List<AccountHistory> findByPersonalAccountId(@Param("personalAccountId") String personalAccountId);
+    Page<AccountHistory> findByPersonalAccountId(@Param("personalAccountId") String personalAccountId, Pageable pageable);
+    AccountHistory findByIdAndPersonalAccountId(@Param("id") String id, @Param("personalAccountId") String personalAccountId);
 }
