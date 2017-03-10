40899d480067aa490b9a15926a67521fc8606144
Алексей Заборщиков <zaborshikov@majordomo.ru>
2017-03-10 16:34:52 +0300

    name to personal account base model belongs to

--- src/main/java/ru/majordomo/hms/personmgr/model/ModelBelongsToPersonalAccount.java
+++ src/main/java/ru/majordomo/hms/personmgr/model/ModelBelongsToPersonalAccount.java
@@ -1,5 +1,6 @@
 package ru.majordomo.hms.personmgr.model;
 
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 import javax.validation.constraints.NotNull;
@@ -15,6 +16,9 @@ public class ModelBelongsToPersonalAccount extends BaseModel {
     @ObjectId(PersonalAccount.class)
     private String personalAccountId;
 
+    @Transient
+    private String personalAccountName;
+
     public ModelBelongsToPersonalAccount() {
         super();
     }
@@ -25,6 +29,14 @@ public class ModelBelongsToPersonalAccount extends BaseModel {
 
     public void setPersonalAccountId(String personalAccountId) {
         this.personalAccountId = personalAccountId;
+    }
+
+    public String getPersonalAccountName() {
+        return personalAccountName;
+    }
+
+    public void setPersonalAccountName(String personalAccountName) {
+        this.personalAccountName = personalAccountName;
     }
 
     @Override
