2f2c461cf640689a925f22b80c3db5f215f21426
Влади́слав Пинаев <pinaev@majordomo.ru>
2017-04-12 17:46:56 +0300

    Убрано временное

--- src/main/java/ru/majordomo/hms/personmgr/service/importing/PersonalAccountDBImportService.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/importing/PersonalAccountDBImportService.java
@@ -41,7 +41,7 @@ public class PersonalAccountDBImportService {
                 "LEFT JOIN Money m ON a.id = m.acc_id " +
                 "LEFT JOIN client c ON a.client_id = c.Client_ID " +
                 "LEFT JOIN extend e ON (a.id = e.acc_id AND e.usluga = 18) " +
-                "WHERE a.client_id != 0 AND a.id > 152327 " +
+                "WHERE a.client_id != 0 " +
                 "ORDER BY a.id ASC";
         jdbcTemplate.query(query, this::rowMap);
     }
@@ -100,7 +100,7 @@ public class PersonalAccountDBImportService {
     }
 
     public boolean importToMongo() {
-        //personalAccountRepository.deleteAll();
+        personalAccountRepository.deleteAll();
         pull();
         pushToMongo();
         return true;
