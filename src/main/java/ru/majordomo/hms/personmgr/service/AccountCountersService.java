5e31d202e11ebdb24923f5b7e3582bc82bec5537
Алексей Заборщиков <zaborshikov@majordomo.ru>
2017-04-26 14:37:39 +0300

    logging

--- src/main/java/ru/majordomo/hms/personmgr/service/AccountCountersService.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/AccountCountersService.java
@@ -1,5 +1,7 @@
 package ru.majordomo.hms.personmgr.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.majordomo.hms.rc.user.resources.Database;
@@ -13,6 +15,8 @@ import java.util.List;
 @Service
 public class AccountCountersService {
     private final RcUserFeignClient rcUserFeignClient;
+
+    private final static Logger logger = LoggerFactory.getLogger(AccountCountersService.class);
 
     @Autowired
     public AccountCountersService(RcUserFeignClient rcUserFeignClient) {
@@ -32,12 +36,15 @@ public class AccountCountersService {
     }
 
     public Long getCurrentQuotaUsed(String accountId) {
-//        Collection<UnixAccount> unixAccounts = rcUserFeignClient.getUnixAccounts(accountId);
         Long currentQuota = 0L;
-//        for (Quotable item : unixAccounts) {
-//            Long itemQuota = item.getQuotaUsed() == null ? 0L : item.getQuotaUsed();
-//            currentQuota += itemQuota;
-//        }
+
+        logger.info("fetching unixAccounts for ID: " + accountId);
+
+        Collection<UnixAccount> unixAccounts = rcUserFeignClient.getUnixAccounts(accountId);
+        for (Quotable item : unixAccounts) {
+            Long itemQuota = item.getQuotaUsed() == null ? 0L : item.getQuotaUsed();
+            currentQuota += itemQuota;
+        }
         Collection<Database> databases = rcUserFeignClient.getDatabases(accountId);
         for (Quotable item : databases) {
             Long itemQuota = item.getQuotaUsed() == null ? 0L : item.getQuotaUsed();
