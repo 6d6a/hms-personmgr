e6ef95262faccb3dbbc30953fd53c37b83135b01
Ilya Dolnigin <dolnigin@majordomo.ru>
2017-04-30 13:49:09 +0300

    Some exception fixes

--- src/main/java/ru/majordomo/hms/personmgr/service/BlackListService.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/BlackListService.java
@@ -1,20 +1,22 @@
 package ru.majordomo.hms.personmgr.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class BlackListService {
     private RestTemplate restTemplate = new RestTemplate();
-    private String fooControlBlackListUrl
-            = "http://78.108.80.183:8080/control";
+    private String fooControlBlackListUrl = "http://78.108.80.183:8080/control";
+//    private String fooControlBlackListUrl = "http://localhost:8080/control";
 
     public Boolean domainExistsInControlBlackList(String domain) {
         Boolean exists = false;
         try {
             exists = restTemplate.getForObject(fooControlBlackListUrl + "/" + domain, Boolean.class);
-        } catch (RestClientException e) {
+            if (exists == null) {
+                return false;
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
