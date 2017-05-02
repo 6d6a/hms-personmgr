be0bad415ea5be4d75972ccc9f1168eeccf61ddf
Ilya Dolnigin <dolnigin@majordomo.ru>
2017-05-02 11:03:42 +0300

    mailmanager updates2

--- src/main/java/ru/majordomo/hms/personmgr/service/MailManager.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/MailManager.java
@@ -20,13 +20,19 @@ public class MailManager {
 
     private final static Logger logger = LoggerFactory.getLogger(MailManager.class);
 
-    private static final HashMap<String, String> URL_MAP;
+    private static final HashMap<UrlKey, String> URL_MAP;
+
+    private enum UrlKey {
+        LOGIN,
+        SEND_EMAIL,
+        SEND_SMS
+    }
 
     static {
         URL_MAP = new HashMap<>();
-        URL_MAP.put("login", "/login_check");
-        URL_MAP.put("sendEmail", "/newmailtask");
-        URL_MAP.put("sendSms", "/smsqueue");
+        URL_MAP.put(UrlKey.LOGIN, "/login_check");
+        URL_MAP.put(UrlKey.SEND_EMAIL, "/newmailtask");
+        URL_MAP.put(UrlKey.SEND_SMS, "/smsqueue");
     }
 
     private RestTemplate restTemplate;
@@ -59,7 +65,7 @@ public class MailManager {
     private String getToken() {
         HttpEntity<String> entity = new HttpEntity<>(credentials, headers);
         ResponseEntity<HashMap> someResponse = restTemplate.postForEntity(
-                URL_ROOT + URL_MAP.get("login"),
+                URL_ROOT + URL_MAP.get(UrlKey.LOGIN),
                 entity,
                 HashMap.class
         );
@@ -69,57 +75,35 @@ public class MailManager {
         return token;
     }
 
-    public HashMap sendEmail(SimpleServiceMessage message) throws RestClientException {
-        checkToken();
-        HttpEntity entity = new HttpEntity<>(message.getParams(), headers);
-        logger.debug("HttpEntity in mailManager sendEmail: " + entity.toString());
-
-        HashMap<String, String> responseBody = new HashMap<>();
-
-        try {
-            ResponseEntity<HashMap> response = restTemplate.postForEntity(
-                    URL_ROOT + URL_MAP.get("sendEmail"),
-                    entity,
-                    HashMap.class
-            );
-
-            return response.getBody();
-        } catch (HttpClientErrorException e) {
-            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-                unSetToken();
-                sendEmail(message);
-            }
-        } catch (RestClientException e) {
-            e.printStackTrace();
-        }
-
-        return responseBody;
+    public HashMap sendEmail(SimpleServiceMessage message) {
+        return send(message, UrlKey.SEND_EMAIL);
     }
 
-    public HashMap sendSms(SimpleServiceMessage message) throws RestClientException {
+    public HashMap sendSms(SimpleServiceMessage message) {
+        return send(message, UrlKey.SEND_SMS);
+    }
+
+    private HashMap send(SimpleServiceMessage message, UrlKey urlKey) {
         checkToken();
         HttpEntity entity = new HttpEntity<>(message.getParams(), headers);
-        logger.debug("HttpEntity in mailManager sendSms: " + entity.toString());
+        logger.debug("HttpEntity in mailManager " + urlKey.name() + ": " + entity.toString());
 
         HashMap<String, String> responseBody = new HashMap<>();
 
         try {
             ResponseEntity<HashMap> response = restTemplate.postForEntity(
-                    URL_ROOT + URL_MAP.get("sendSms"),
+                    URL_ROOT + URL_MAP.get(urlKey),
                     entity,
                     HashMap.class
             );
-
-            if (response.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-                unSetToken();
-                sendSms(message);
-            }
 
             return response.getBody();
         } catch (HttpClientErrorException e) {
             if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
                 unSetToken();
-                sendEmail(message);
+                send(message, urlKey);
+            } else {
+                e.printStackTrace();
             }
         } catch (RestClientException e) {
             e.printStackTrace();
