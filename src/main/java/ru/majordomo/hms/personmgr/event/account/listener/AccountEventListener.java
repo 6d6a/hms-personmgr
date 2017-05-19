851a7b468236e5d4d83f0fb9374db6cae3b87eb8
Влади́слав Пинаев <pinaev@majordomo.ru>
2017-05-19 12:32:57 +0300

    Бросаем эвенты с задержками при поступлении сообщения о платеже

--- src/main/java/ru/majordomo/hms/personmgr/event/account/listener/AccountEventListener.java
+++ src/main/java/ru/majordomo/hms/personmgr/event/account/listener/AccountEventListener.java
@@ -8,10 +8,12 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+import ru.majordomo.hms.personmgr.common.AccountStatType;
 import ru.majordomo.hms.personmgr.common.MailManagerMessageType;
 import ru.majordomo.hms.personmgr.common.TokenType;
 import ru.majordomo.hms.personmgr.common.Utils;
@@ -19,18 +21,23 @@ import ru.majordomo.hms.personmgr.common.message.SimpleServiceMessage;
 import ru.majordomo.hms.personmgr.event.account.*;
 import ru.majordomo.hms.personmgr.event.accountHistory.AccountHistoryEvent;
 import ru.majordomo.hms.personmgr.event.mailManager.SendMailEvent;
+import ru.majordomo.hms.personmgr.model.AccountStat;
 import ru.majordomo.hms.personmgr.model.PersonalAccount;
+import ru.majordomo.hms.personmgr.model.plan.Plan;
+import ru.majordomo.hms.personmgr.model.promocode.AccountPromocode;
+import ru.majordomo.hms.personmgr.model.promotion.AccountPromotion;
+import ru.majordomo.hms.personmgr.model.promotion.Promotion;
+import ru.majordomo.hms.personmgr.repository.*;
+import ru.majordomo.hms.personmgr.service.AbonementService;
 import ru.majordomo.hms.personmgr.service.AccountHelper;
+import ru.majordomo.hms.personmgr.service.FinFeignClient;
 import ru.majordomo.hms.personmgr.service.TokenHelper;
 import ru.majordomo.hms.rc.user.resources.Domain;
 
-import static ru.majordomo.hms.personmgr.common.Constants.HISTORY_MESSAGE_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.IP_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.OPERATOR_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.PASSWORD_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.SERVICE_NAME_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.TECHNICAL_SUPPORT_EMAIL;
-import static ru.majordomo.hms.personmgr.common.Constants.TOKEN_KEY;
+import static ru.majordomo.hms.personmgr.common.AccountSetting.CREDIT_ACTIVATION_DATE;
+import static ru.majordomo.hms.personmgr.common.Constants.*;
+import static ru.majordomo.hms.personmgr.common.Constants.BONUS_PARTNER_PERCENT;
+import static ru.majordomo.hms.personmgr.common.Constants.BONUS_PARTNER_TYPE_ID;
 
 @Component
 public class AccountEventListener {
@@ -39,16 +46,40 @@ public class AccountEventListener {
     private final AccountHelper accountHelper;
     private final TokenHelper tokenHelper;
     private final ApplicationEventPublisher publisher;
+    private final AccountPromocodeRepository accountPromocodeRepository;
+    private final FinFeignClient finFeignClient;
+    private final AccountStatRepository accountStatRepository;
+    private final PlanRepository planRepository;
+    private final AccountPromotionRepository accountPromotionRepository;
+    private final PromotionRepository promotionRepository;
+    private final AbonementService abonementService;
+    private final PersonalAccountRepository accountRepository;
 
     @Autowired
     public AccountEventListener(
             AccountHelper accountHelper,
             TokenHelper tokenHelper,
-            ApplicationEventPublisher publisher
+            ApplicationEventPublisher publisher,
+            AccountPromocodeRepository accountPromocodeRepository,
+            FinFeignClient finFeignClient,
+            AccountStatRepository accountStatRepository,
+            PlanRepository planRepository,
+            AccountPromotionRepository accountPromotionRepository,
+            PromotionRepository promotionRepository,
+            AbonementService abonementService,
+            PersonalAccountRepository accountRepository
     ) {
         this.accountHelper = accountHelper;
         this.tokenHelper = tokenHelper;
         this.publisher = publisher;
+        this.accountPromocodeRepository = accountPromocodeRepository;
+        this.finFeignClient = finFeignClient;
+        this.accountStatRepository = accountStatRepository;
+        this.planRepository = planRepository;
+        this.accountPromotionRepository = accountPromotionRepository;
+        this.promotionRepository = promotionRepository;
+        this.abonementService = abonementService;
+        this.accountRepository = accountRepository;
     }
 
     @EventListener
@@ -311,6 +342,163 @@ public class AccountEventListener {
             message.addParam("parametrs", parameters);
 
             publisher.publishEvent(new SendMailEvent(message));
+        }
+    }
+
+    @EventListener
+    @Async("threadPoolTaskExecutor")
+    public void onAccountPromotionProcessByPaymentCreatedEvent(AccountPromotionProcessByPaymentCreatedEvent event) {
+
+        // Задержка
+        try {
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            logger.debug("Exeption in AccountEventListener on sleep");
+            e.printStackTrace();
+        }
+
+        PersonalAccount account = event.getSource();
+        Map<String, ?> paramsForPublisher = event.getParams();
+
+        logger.debug("We got AccountPromotionProcessByPaymentCreatedEvent");
+
+        // Пополнение баланса партнера при поступлении средств клиенту, если этот клиент регистрировался по промокоду
+
+        BigDecimal amount = new BigDecimal((Integer) paramsForPublisher.get("amount"));
+        Plan plan = planRepository.findOne(account.getPlanId());
+        // При открытии нового аккаунта виртуального хостинга по тарифным планам «Безлимитный», «Безлимитный+», «Бизнес», «Бизнес+»
+        // мы бесплатно зарегистрируем на Вас 1 домен в зоне .ru или .рф при единовременной оплате за
+        // 3 месяца. Бонус предоставляется при открытии аккаунта для первого домена на аккаунте.
+        if (amount.compareTo((plan.getService().getCost()).multiply(new BigDecimal(3L))) >= 0) {
+            //Проверка на то что аккаунт новый (на нём не было доменов)
+            if (account.isAccountNew()) {
+                List<Domain> domains = accountHelper.getDomains(account);
+                if (!plan.isAbonementOnly() && plan.isActive() && (domains == null || domains.size() == 0)) {
+                    Promotion promotion = promotionRepository.findByName(FREE_DOMAIN_PROMOTION);
+                    List<AccountPromotion> accountPromotions = accountPromotionRepository.findByPersonalAccountIdAndPromotionId(account.getId(), promotion.getId());
+                    if (accountPromotions == null || accountPromotions.isEmpty()) {
+                        accountHelper.giveGift(account, promotion);
+                    }
+                }
+            }
+        }
+
+        if (accountPromocodeRepository.countByPersonalAccountIdAndOwnedByAccount(account.getId(), false) > 1) {
+            logger.error("Account has more than one AccountPromocodes with OwnedByAccount == false. Id: " + account.getId());
+            return;
+        }
+
+        // Проверка на то что аккаунт создан по партнерскому промокоду
+        AccountPromocode accountPromocode = accountPromocodeRepository.findOneByPersonalAccountIdAndOwnedByAccount(account.getId(), false);
+
+        if (accountPromocode != null) {
+
+            // Аккаунт которому необходимо начислить средства
+            PersonalAccount accountForPartnerBonus = accountRepository.findByAccountId(accountPromocode.getOwnerPersonalAccountId());
+
+            if (accountForPartnerBonus == null) {
+                logger.error("PersonalAccount with ID: " + accountPromocode.getOwnerPersonalAccountId() + " not found.");
+                return;
+            }
+
+            // Проверка даты создания аккаунта
+            if (account.getCreated().isBefore(accountForPartnerBonus.getCreated().plusYears(1))) {
+                // Все условия выполнены
+
+                BigDecimal percent = new BigDecimal(BONUS_PARTNER_PERCENT);
+
+                BigDecimal promocodeBonus = amount.multiply(percent);
+
+                Map<String, Object> payment = new HashMap<>();
+                payment.put("accountId", accountForPartnerBonus.getName());
+                payment.put("paymentTypeId", BONUS_PARTNER_TYPE_ID);
+                payment.put("amount", promocodeBonus);
+                payment.put("message", "Бонусный платеж за использование промокода " + accountPromocode.getPromocode().getCode() + " на аккаунте: " + account.getName());
+
+                try {
+                    String responseMessage = finFeignClient.addPayment(payment);
+                    logger.debug("Processed promocode addPayment: " + responseMessage);
+
+                    //Save history
+                    Map<String, String> params = new HashMap<>();
+                    params.put(HISTORY_MESSAGE_KEY, "Произведено начисление процента от пополнения (" + promocodeBonus.toString() + " руб. от " + amount.toString() + " руб.) владельцу партнерского промокода" + accountPromocode.getPromocode().getCode() + " - " + accountForPartnerBonus.getName());
+                    params.put(OPERATOR_KEY, "service");
+
+                    publisher.publishEvent(new AccountHistoryEvent(account.getId(), params));
+
+                    //Статистика
+                    AccountStat accountStat = new AccountStat();
+                    accountStat.setPersonalAccountId(accountForPartnerBonus.getId());
+                    accountStat.setCreated(LocalDateTime.now());
+                    accountStat.setType(AccountStatType.VIRTUAL_HOSTING_PARTNER_PROMOCODE_BALANCE_FILL);
+
+                    Map<String, String> data = new HashMap<>();
+                    data.put("usedByPersonalAccountId", account.getId());
+                    data.put("usedByPersonalAccountName", account.getName());
+                    data.put("amount", String.valueOf(promocodeBonus));
+
+                    accountStat.setData(data);
+
+                    accountStatRepository.save(accountStat);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    logger.error("Exception in pm.payment.create AMQP listener: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    @EventListener
+    @Async("threadPoolTaskExecutor")
+    public void onAccountSwitchByPaymentCreatedEvent(AccountSwitchByPaymentCreatedEvent event) {
+
+        // Задержка (К примеру, в случае возврата денег в процессе смены тарифа)
+        try {
+            Thread.sleep(20000);
+        } catch (Exception e) {
+            logger.debug("Exeption in AccountEventListener on sleep");
+            e.printStackTrace();
+        }
+
+        PersonalAccount account = event.getSource();
+
+        logger.debug("We got AccountSwitchByPaymentCreatedEvent");
+
+        // Если баланс после пополнения положительный
+        BigDecimal balance = accountHelper.getBalance(account);
+        Plan plan = planRepository.findOne(account.getPlanId());
+
+        if (!plan.isAbonementOnly()) {
+
+            if (balance.compareTo(BigDecimal.ZERO) > 0) {
+                // Обнуляем дату активации кредита
+                if (account.getCreditActivationDate() != null) {
+                    account.removeSettingByName(CREDIT_ACTIVATION_DATE);
+                    accountRepository.save(account);
+                }
+
+                // Включаем аккаунт, если был выключен
+                if (!account.isActive()) {
+                    accountHelper.switchAccountResources(account, true);
+                    // сразу списываем за текущий день после включения (если не хватает - аккаунт снова выключится)
+                    publisher.publishEvent(new AccountProcessChargesEvent(account));
+                }
+            }
+
+        } else {
+
+            String addAbonementId = plan.getNotInternalAbonementId();
+
+            if (addAbonementId != null) {
+                try {
+                    abonementService.addAbonement(account, addAbonementId, true, false, false);
+                    accountHelper.switchAccountResources(account, true);
+                } catch (Exception e) {
+                    logger.info("Ошибка при покупке абонемента для AbonementOnly плана.");
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
