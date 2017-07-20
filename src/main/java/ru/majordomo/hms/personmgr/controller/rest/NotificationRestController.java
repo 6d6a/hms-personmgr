89bca4bbca3eb594fa8ba8b32e3f209316df3c98
Валентин Березка <berezka@majordomo.ru>
2017-07-20 16:25:52 +0300

    Update NotificationRestController.java

--- src/main/java/ru/majordomo/hms/personmgr/controller/rest/NotificationRestController.java
+++ src/main/java/ru/majordomo/hms/personmgr/controller/rest/NotificationRestController.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -73,5 +76,4 @@ public class NotificationRestController extends CommonRestController {
 
         return new ResponseEntity<>(notifications, HttpStatus.OK);
     }
-
 }
