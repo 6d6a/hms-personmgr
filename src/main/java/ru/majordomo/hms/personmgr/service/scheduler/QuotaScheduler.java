249f1e02255464ac67c49581b64b5a1711422185
Березка Валентин <berezka@majordomo.ru>
2017-08-15 12:03:02 +0300

    scheduler

--- src/main/java/ru/majordomo/hms/personmgr/service/scheduler/QuotaScheduler.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/scheduler/QuotaScheduler.java
@@ -34,14 +34,13 @@ public class QuotaScheduler {
     }
 
     //Выполняем проверку квоты каждые 30 минут
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0 */30 * * * *")
     @SchedulerLock(name = "processQuotaChecks")
     public void processQuotaChecks() {
         logger.debug("Started processQuotaChecks");
-//        try (Stream<PersonalAccount> personalAccountStream = accountManager.findByIdNotIn(Collections.singletonList(TECHNICAL_ACCOUNT_ID))) {
-//            personalAccountStream.forEach(account -> publisher.publishEvent(new AccountCheckQuotaEvent(account)));
-//        }
-        publisher.publishEvent(new AccountCheckQuotaEvent(accountManager.findOne("5947efb097a1c000063eb492")));
+        try (Stream<PersonalAccount> personalAccountStream = accountManager.findByIdNotIn(Collections.singletonList(TECHNICAL_ACCOUNT_ID))) {
+            personalAccountStream.forEach(account -> publisher.publishEvent(new AccountCheckQuotaEvent(account)));
+        }
         logger.debug("Ended processQuotaChecks");
     }
 }
