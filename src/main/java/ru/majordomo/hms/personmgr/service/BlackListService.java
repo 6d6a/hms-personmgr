e900dcdbd6f452d66571536445fdf3190244aba8
Влади́слав Пинаев <pinaev@majordomo.ru>
2017-12-13 16:23:14 +0300

    issue_8307

--- src/main/java/ru/majordomo/hms/personmgr/service/BlackListService.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/BlackListService.java
@@ -15,22 +15,32 @@ public class BlackListService {
     private RestTemplate restTemplate = new RestTemplate();
 
     private String controlBlackListUrl;
+    private String control2BlackListUrl;
 
     @Value("${blacklist.control_url}")
     public void setControlBlackListUrl(String controlBlackListUrl) {
         this.controlBlackListUrl = controlBlackListUrl;
     }
 
+    @Value("${blacklist.control2_url}")
+    public void setControl2BlackListUrl(String control2BlackListUrl) {
+        this.control2BlackListUrl = control2BlackListUrl;
+    }
+
     public Boolean domainExistsInControlBlackList(String domain) {
         Boolean exists = false;
         try {
             exists = restTemplate.getForObject(controlBlackListUrl + "/" + domain, Boolean.class);
-            if (exists == null) {
-                return false;
-            }
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("Exception in BlackListService.domainExistsInControlBlackList " + e.getMessage());
+        }
+
+        try {
+            exists = restTemplate.getForObject(control2BlackListUrl + "/" + domain, Boolean.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Exception in BlackListService.domainExistsInControl2BlackList " + e.getMessage());
         }
 
         return exists;
