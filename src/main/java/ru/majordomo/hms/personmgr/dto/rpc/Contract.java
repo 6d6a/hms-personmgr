11851dc630ef07f1aefece22f6bac4200dce35fa
Березка Валентин <berezka@majordomo.ru>
2018-01-19 16:53:16 +0300

    удалены лишние переменные

--- src/main/java/ru/majordomo/hms/personmgr/dto/rpc/Contract.java
+++ src/main/java/ru/majordomo/hms/personmgr/dto/rpc/Contract.java
@@ -1,17 +1,17 @@
 package ru.majordomo.hms.personmgr.dto.rpc;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import ru.majordomo.hms.personmgr.common.Utils;
 
 import java.io.UnsupportedEncodingException;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@JsonIgnoreProperties(value = { "updated", "operator_id" })
 @Data
 public class Contract {
 
@@ -20,21 +20,11 @@ public class Contract {
     private Boolean status; //'active', 'archived', 'draft'
     private String type; //'oferta', 'company', 'entrepreneur', 'oferta_virtual_hosting', 'virtual_hosting'
 
-    @JsonIgnore
-    private LocalDateTime updated;
-
     @JsonProperty("no_footer_pages")
     private List<Integer> noFooterPages; //no_footer_pages": "3,5,7",
 
-    @JsonProperty("operator_id")
-    private Integer operatorId; //operator_id": "41",
-
     @JsonProperty("contract_id")
     private Integer contractId; //contract_id": "128",
-
-    public String getBody() {
-        return body;
-    }
 
     public void setBody(String body) {
         try {
@@ -44,10 +34,6 @@ public class Contract {
         }
     }
 
-    public String getFooter() {
-        return footer;
-    }
-
     public void setFooter(String footer) {
         try {
             this.footer = Utils.convertToUTF8(footer, "ISO-8859-1");
@@ -55,46 +41,6 @@ public class Contract {
             this.footer = footer;
         }
     }
-
-//    public Integer getOperatorId() {
-//        return operatorId;
-//    }
-//
-//    public void setOperatorId(Integer operatorId) {
-//        this.operatorId = operatorId;
-//    }
-//
-//    public Boolean getStatus() {
-//        return status;
-//    }
-//
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-//
-//    public String getType() {
-//        return type;
-//    }
-//
-//    public void setType(String type) {
-//        this.type = type;
-//    }
-//
-//    public Integer getContractId() {
-//        return contractId;
-//    }
-//
-//    public void setContractId(Integer contractId) {
-//        this.contractId = contractId;
-//    }
-//
-//    public List<Integer> getNoFooterPages() {
-//        return noFooterPages;
-//    }
-//
-//    public void setNoFooterPages(List<Integer> noFooterPages) {
-//        this.noFooterPages = noFooterPages;
-//    }
 
     public void setNoFooterPages(String row) {
         if (row == null || row.length() == 0) {
@@ -109,14 +55,6 @@ public class Contract {
     }
 
     public void setStatus(String status){
-        setStatus(status.equals("active"));
-    }
-
-    public LocalDateTime getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(LocalDateTime updated) {
-        this.updated = updated;
+        this.status = status.equals("active");
     }
 }
