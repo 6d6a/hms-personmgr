a79d6950cacda54717d326de8fe4ba0bb21dd724
Березка Валентин <berezka@majordomo.ru>
2018-03-01 17:29:23 +0300

    правки api AppsCatRestController PersonalAccountRestController

--- src/main/java/ru/majordomo/hms/personmgr/controller/rest/AppsCatRestController.java
+++ src/main/java/ru/majordomo/hms/personmgr/controller/rest/AppsCatRestController.java
@@ -1,6 +1,7 @@
 package ru.majordomo.hms.personmgr.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.validation.annotation.Validated;
@@ -9,13 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
-
 import ru.majordomo.hms.personmgr.common.message.SimpleServiceMessage;
-import ru.majordomo.hms.personmgr.event.accountHistory.AccountHistoryEvent;
 import ru.majordomo.hms.personmgr.exception.ParameterValidationException;
 import ru.majordomo.hms.personmgr.model.account.PersonalAccount;
 import ru.majordomo.hms.personmgr.model.business.ProcessingBusinessAction;
@@ -23,8 +18,6 @@ import ru.majordomo.hms.personmgr.service.AppsCatService;
 import ru.majordomo.hms.personmgr.validation.ObjectId;
 
 import static ru.majordomo.hms.personmgr.common.Constants.ACCOUNT_ID_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.HISTORY_MESSAGE_KEY;
-import static ru.majordomo.hms.personmgr.common.Constants.OPERATOR_KEY;
 
 @RestController
 @Validated
@@ -37,10 +30,9 @@ public class AppsCatRestController extends CommonRestController {
     }
 
     @PostMapping("/{accountId}/app_install")
-    public SimpleServiceMessage create(
+    public ResponseEntity<SimpleServiceMessage> create(
             @RequestBody SimpleServiceMessage message,
             @ObjectId(PersonalAccount.class) @PathVariable(value = "accountId") String accountId,
-            HttpServletResponse response,
             SecurityContextHolderAwareRequestWrapper request,
             Authentication authentication
     ) {
@@ -58,23 +50,11 @@ public class AppsCatRestController extends CommonRestController {
 
         ProcessingBusinessAction businessAction;
 
-        try {
-            businessAction = appsCatService.processInstall(message);
-        } catch (Exception e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return this.createErrorResponse(e.getMessage());
-        }
+        businessAction = appsCatService.processInstall(message);
 
-        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+        saveHistory(request, accountId,
+                "Поступила заявка на установку приложения для сайта (имя: " + message.getParam("name") + ")");
 
-        //Save history
-        String operator = request.getUserPrincipal() != null ? request.getUserPrincipal().getName() : "service";
-        Map<String, String> params = new HashMap<>();
-        params.put(HISTORY_MESSAGE_KEY, "Поступила заявка на установку приложения для сайта (имя: " + message.getParam("name") + ")");
-        params.put(OPERATOR_KEY, operator);
-
-        publisher.publishEvent(new AccountHistoryEvent(accountId, params));
-
-        return this.createSuccessResponse(businessAction);
+        return ResponseEntity.accepted().body(createSuccessResponse(businessAction));
     }
 }
