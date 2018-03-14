7815593821a75642e07d17abc8caed75e1fc061e
Березка Валентин <berezka@majordomo.ru>
2018-03-14 17:03:00 +0300

    statFeignClient

--- src/main/java/ru/majordomo/hms/personmgr/service/StatFeignClient.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/StatFeignClient.java
@@ -1,10 +1,8 @@
 package ru.majordomo.hms.personmgr.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import ru.majordomo.hms.personmgr.common.message.SimpleServiceMessage;
 import ru.majordomo.hms.personmgr.config.FeignConfig;
 
 import java.util.Map;
@@ -12,8 +10,11 @@ import java.util.Map;
 @FeignClient(name = "stat", configuration = FeignConfig.class)
 public interface StatFeignClient {
     @RequestMapping(method = RequestMethod.POST, value = "/collector/notifications/increment", consumes = "application/json")
-    void sendNotificaton(Map<String, Object> body);
+    void notificatonWasSendIncrement(Map<String, Object> body);
 
     @RequestMapping(method = RequestMethod.POST, value = "/collector/payment-after-notification/increment", consumes = "application/json")
     void paymentAfterNotificationIncrement(Map<String, Object> body);
+
+    @RequestMapping(method = RequestMethod.POST, value = "/collector/user-disabled-service-after-notification/increment", consumes = "application/json")
+    void userDisabledServiceAfterNotificationIncrement(Map<String, Object> body);
 }
