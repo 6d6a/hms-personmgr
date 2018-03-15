f315f95984348924ad943e863ebac8b0b3c12692
Влади́слав Пинаев <pinaev@majordomo.ru>
2018-03-15 18:11:55 +0300

    Добавлен JsonIgnorePropertie

--- src/main/java/ru/majordomo/hms/personmgr/dto/revisium/ApiResponse.java
+++ src/main/java/ru/majordomo/hms/personmgr/dto/revisium/ApiResponse.java
@@ -1,11 +1,13 @@
 package ru.majordomo.hms.personmgr.dto.revisium;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
+@JsonIgnoreProperties(ignoreUnknown = true)
 class ApiResponse {
 
     @JsonProperty("status")
--- src/main/java/ru/majordomo/hms/personmgr/dto/revisium/GetStatResponse.java
+++ src/main/java/ru/majordomo/hms/personmgr/dto/revisium/GetStatResponse.java
@@ -1,11 +1,13 @@
 package ru.majordomo.hms.personmgr.dto.revisium;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GetStatResponse extends ApiResponse {
 
     @JsonProperty("balance")
