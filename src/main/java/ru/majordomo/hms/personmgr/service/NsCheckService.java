de4fe22e7249b2efd6840d94829207700de528f4
Березка Валентин <berezka@majordomo.ru>
2018-05-21 13:38:25 +0300

    проверка ns домена вынесена в сервис

--- src/main/java/ru/majordomo/hms/personmgr/service/NsCheckService.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/NsCheckService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xbill.DNS.*;
+import ru.majordomo.hms.personmgr.exception.ParameterValidationException;
 import ru.majordomo.hms.rc.user.resources.Domain;
 
 import java.net.IDN;
@@ -23,38 +24,43 @@ public class NsCheckService {
     }
 
     public boolean checkOurNs(Domain domain) {
-        Boolean canOrderSSL = false;
-        Boolean hasAlienNS = false;
-
-        try {
-            if (domain.getParentDomainId() != null) {
-                Domain originDomain = rcUserFeignClient.getDomain(domain.getAccountId(), domain.getParentDomainId());
-                return checkOurNs(originDomain);
+        if (domain.getParentDomainId() != null) {
+            Domain originDomain;
+            try {
+                originDomain = rcUserFeignClient.getDomain(domain.getAccountId(), domain.getParentDomainId());
+            } catch (Exception e) {
+                throw new ParameterValidationException("Основной домен для поддомена не найден на вашем аккаунте");
             }
+            return checkOurNs(originDomain);
+        } else {
+            try {
+                Boolean canOrderSSL = false;
+                Boolean hasAlienNS = false;
 
-            Lookup lookup = new Lookup(IDN.toASCII(domain.getName()), Type.NS);
-            lookup.setResolver(new SimpleResolver("8.8.8.8"));
-            lookup.setCache(null);
+                Lookup lookup = new Lookup(IDN.toASCII(domain.getName()), Type.NS);
+                lookup.setResolver(new SimpleResolver("8.8.8.8"));
+                lookup.setCache(null);
 
-            Record[] records = lookup.run();
+                Record[] records = lookup.run();
 
-            if (records != null) {
-                for (Record record : records) {
-                    NSRecord nsRecord = (NSRecord) record;
-                    if (nsRecord.getTarget().equals(Name.fromString("ns.majordomo.ru.")) ||
-                            nsRecord.getTarget().equals(Name.fromString("ns2.majordomo.ru.")) ||
-                            nsRecord.getTarget().equals(Name.fromString("ns3.majordomo.ru.")) ) {
-                        canOrderSSL = true;
-                    } else {
-                        hasAlienNS = true;
+                if (records != null) {
+                    for (Record record : records) {
+                        NSRecord nsRecord = (NSRecord) record;
+                        if (nsRecord.getTarget().equals(Name.fromString("ns.majordomo.ru.")) ||
+                                nsRecord.getTarget().equals(Name.fromString("ns2.majordomo.ru.")) ||
+                                nsRecord.getTarget().equals(Name.fromString("ns3.majordomo.ru."))) {
+                            canOrderSSL = true;
+                        } else {
+                            hasAlienNS = true;
+                        }
                     }
                 }
+                return canOrderSSL && !hasAlienNS;
+            } catch (UnknownHostException | TextParseException e) {
+                logger.error("Ошибка при получении NS-записей: " + e.getMessage());
+                e.printStackTrace();
+                return false;
             }
-        } catch (UnknownHostException | TextParseException e) {
-            logger.error("Ошибка при получении NS-записей: " + e.getMessage());
-            e.printStackTrace();
         }
-
-        return canOrderSSL && !hasAlienNS;
     }
 }
