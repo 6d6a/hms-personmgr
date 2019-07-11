4a8e5ba9ba2eaa178ff2fd873155245c6f688c95
Березка Валентин <berezka@majordomo.ru>
2019-07-11 16:46:11 +0300

    add lombok @data to accountA

--- src/main/java/ru/majordomo/hms/personmgr/model/abonement/AccountAbonement.java
+++ src/main/java/ru/majordomo/hms/personmgr/model/abonement/AccountAbonement.java
@@ -1,7 +1,8 @@
 package ru.majordomo.hms.personmgr.model.abonement;
 
 
-import org.springframework.data.annotation.PersistenceConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,8 +13,9 @@ import javax.validation.constraints.NotNull;
 
 import ru.majordomo.hms.personmgr.model.VersionedModelBelongsToPersonalAccount;
 import ru.majordomo.hms.personmgr.validation.ObjectId;
-import ru.majordomo.hms.personmgr.validation.UniquePersonalAccountIdModel;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 @Document
 public class AccountAbonement extends VersionedModelBelongsToPersonalAccount {
     @NotNull
@@ -31,68 +33,4 @@ public class AccountAbonement extends VersionedModelBelongsToPersonalAccount {
 
     @Transient
     private Abonement abonement;
-
-    public AccountAbonement() {
-    }
-
-    @PersistenceConstructor
-    public AccountAbonement(String id, String abonementId, LocalDateTime created, LocalDateTime expired, boolean autorenew) {
-        super();
-        this.setId(id);
-        this.abonementId = abonementId;
-        this.created = created;
-        this.expired = expired;
-        this.autorenew = autorenew;
-    }
-
-    public String getAbonementId() {
-        return abonementId;
-    }
-
-    public void setAbonementId(String abonementId) {
-        this.abonementId = abonementId;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
-
-    public LocalDateTime getExpired() {
-        return expired;
-    }
-
-    public void setExpired(LocalDateTime expired) {
-        this.expired = expired;
-    }
-
-    public boolean isAutorenew() {
-        return autorenew;
-    }
-
-    public void setAutorenew(boolean autorenew) {
-        this.autorenew = autorenew;
-    }
-
-    public Abonement getAbonement() {
-        return abonement;
-    }
-
-    public void setAbonement(Abonement abonement) {
-        this.abonement = abonement;
-    }
-
-    @Override
-    public String toString() {
-        return "AccountAbonement{" +
-                "abonementId='" + abonementId + '\'' +
-                ", created=" + created +
-                ", expired=" + expired +
-                ", autorenew=" + autorenew +
-                ", abonement=" + abonement +
-                "} " + super.toString();
-    }
 }
