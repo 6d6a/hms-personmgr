83b03bb572b7b90139ce63b245934f53008ff98e
foxikk <pinaev@majordomo.ru>
2020-06-16 13:56:45 +0300

    Ещё один фикс квоты

--- src/main/java/ru/majordomo/hms/personmgr/repository/BatchJobRepository.java
+++ src/main/java/ru/majordomo/hms/personmgr/repository/BatchJobRepository.java
@@ -3,11 +3,9 @@ package ru.majordomo.hms.personmgr.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 import ru.majordomo.hms.personmgr.model.batch.BatchJob;
 
 public interface BatchJobRepository extends MongoRepository<BatchJob, String> {
     BatchJob findFirstByRunDateAndTypeOrderByCreatedDesc(LocalDate runDate, BatchJob.Type type);
-    Optional<BatchJob> findFirstByStateAndTypeOrderByCreatedDesc(BatchJob.State state, BatchJob.Type type);
 }
