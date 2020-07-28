45c9b69dddd7273d71d0bcbffcd4b48e401142c8
foxikk <pinaev@majordomo.ru>
2020-07-28 13:15:12 +0300

    Фикс трансфера доменов

--- src/main/java/ru/majordomo/hms/personmgr/feign/DomainRegistrarFeignClient.java
+++ src/main/java/ru/majordomo/hms/personmgr/feign/DomainRegistrarFeignClient.java
@@ -2,6 +2,7 @@ package ru.majordomo.hms.personmgr.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.majordomo.hms.personmgr.common.AvailabilityInfo;
@@ -17,9 +18,9 @@ public interface DomainRegistrarFeignClient {
     @RequestMapping(method = RequestMethod.GET, value = "/domain/{domainName}/get-renew-premium-price", consumes = "application/json")
     BigDecimal getRenewPremiumPrice(@PathVariable("domainName") String domainName);
 
-    @RequestMapping(method = RequestMethod.POST, value = "/person/{nicHandle}/transfer/request/{authInfo}/domain/{domain}", consumes = "application/json")
-    void transferRequest(@PathVariable String nicHandle, @PathVariable String authInfo, @PathVariable String domain);
+    @RequestMapping(method = RequestMethod.POST, value = "/person/{nicHandle}/transfer/request/domain/{domain}", consumes = "application/json")
+    void transferRequest(@PathVariable("nicHandle") String nicHandle, @RequestBody String authInfo, @PathVariable("domain") String domain);
 
     @RequestMapping(method = RequestMethod.POST, value = "/person/{nicHandle}/transfer/confirmation/{verificationCode}", consumes = "application/json")
-    void transferConfirmation(@PathVariable String nicHandle, @PathVariable String verificationCode);
+    void transferConfirmation(@PathVariable("nicHandle") String nicHandle, @PathVariable("verificationCode") String verificationCode);
 }
