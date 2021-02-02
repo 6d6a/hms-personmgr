881754e2cee633597798ca6e7baa198543a847e7
Андрианова Ольга <andrianov@majordomo.ru>
2021-02-02 14:58:12 +0000

    Исправлен класс подсчета стоимости возврата за абонемент.

--- src/main/java/ru/majordomo/hms/personmgr/service/PlanChange/behavior/RegularToRegularCashBackCalculator.java
+++ src/main/java/ru/majordomo/hms/personmgr/service/PlanChange/behavior/RegularToRegularCashBackCalculator.java
@@ -6,6 +6,8 @@ import ru.majordomo.hms.personmgr.model.abonement.AbonementBuyInfo;
 import ru.majordomo.hms.personmgr.model.abonement.AccountAbonement;
 import ru.majordomo.hms.personmgr.model.plan.Plan;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -16,6 +18,7 @@ import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
+@ParametersAreNonnullByDefault
 public class RegularToRegularCashBackCalculator implements CashBackCalculator<AccountAbonement> {
     private final Plan currentPlan;
 
@@ -24,6 +27,13 @@ public class RegularToRegularCashBackCalculator implements CashBackCalculator<Ac
     }
 
     public BigDecimal calc(AccountAbonement currentAccountAbonement) {
+        return calc(currentAccountAbonement, null);
+    }
+
+    public BigDecimal calc(AccountAbonement currentAccountAbonement, @Nullable LocalDateTime currentDateTime) {
+        if (currentDateTime == null) {
+            currentDateTime = LocalDateTime.now();
+        }
         List<AbonementBuyInfo> buyInfo = currentAccountAbonement.getAbonementBuyInfos();
         buyInfo.sort(Comparator.comparing(AbonementBuyInfo::getBuyDate).reversed());
 
@@ -38,19 +48,19 @@ public class RegularToRegularCashBackCalculator implements CashBackCalculator<Ac
             return abonementCost;
         }
 
-        if (currentAccountAbonement.getExpired().isBefore(LocalDateTime.now())) {
+        if (currentAccountAbonement.getExpired().isBefore(currentDateTime)) {
             return BigDecimal.ZERO;
         }
 
         LocalDate accountAbonementExpired = currentAccountAbonement.getExpired().toLocalDate();
         Period abonementPeriod = Period.parse(abonement.getPeriod());
-        LocalDate now = LocalDate.now();
+        LocalDate currentDate = currentDateTime.toLocalDate();
         LocalDate nextDate = accountAbonementExpired; // первая дата для начала пересчета АБ
 
         BigDecimal sumAbCost = BigDecimal.ZERO;
         Iterator<AbonementBuyInfo> iter = buyInfo.iterator();
 
-        while (!nextDate.isEqual(now) && nextDate.isAfter(now)) {
+        while (!nextDate.isEqual(currentDate) && nextDate.isAfter(currentDate)) {
             nextDate = nextDate.minus(abonementPeriod);
             if (iter.hasNext()) {
                 AbonementBuyInfo a = iter.next();
@@ -58,15 +68,15 @@ public class RegularToRegularCashBackCalculator implements CashBackCalculator<Ac
             } else {
                 sumAbCost = sumAbCost.add(abonementCost);
             }
-        }
-
+        };
+        // nextDate тут это дата начала абонемента
         BigDecimal dailyChargesAmount = BigDecimal.ZERO;
         BigDecimal currentPlanCost = currentPlan.getService().getCost();
-        if (!nextDate.isEqual(now)) {
-            while (now.isAfter(nextDate)) {
+        if (!nextDate.isEqual(currentDate)) {
+            while (currentDate.isAfter(nextDate)) {
+                nextDate = nextDate.plusDays(1L);
                 int daysInMonth = nextDate.lengthOfMonth();
                 dailyChargesAmount = dailyChargesAmount.add(currentPlanCost.divide(BigDecimal.valueOf(daysInMonth), 4, BigDecimal.ROUND_HALF_UP));
-                nextDate = nextDate.plusDays(1L);
             }
         }
 
